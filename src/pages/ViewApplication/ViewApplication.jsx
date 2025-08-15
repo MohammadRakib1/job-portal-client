28766c14d79793e900619ecfddb7bcd91f45b80d
MohammadOfficials <mohammadofficial4@gmail.com>
2025-08-15 12:23:07 +0600

    last commit

--- src/pages/ViewApplication/ViewApplication.jsx
+++ src/pages/ViewApplication/ViewApplication.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ViewApplication = () => {
 
     const applications = useLoaderData();
+
+    const handleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id)
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "status has been updated",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    }
 
     return (
         <div>
@@ -26,11 +53,12 @@ const ViewApplication = () => {
                                 <td>{app.applicant_email}</td>
                                 <td>Quality Control Specialist</td>
                                 <td>
-                                    <select className="select select-bordered select-xs w-full max-w-xs">
-                                        <option disabled selected>Tiny</option>
-                                        <option>Tiny Apple</option>
-                                        <option>Tiny Orange</option>
-                                        <option>Tiny Tomato</option>
+                                    <select onChange={(e) => handleStatusUpdate(e, app._id)} className="select select-bordered select-xs w-full max-w-xs">
+                                        <option disabled selected>Change Status</option>
+                                        <option>Under review</option>
+                                        <option>set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
                                     </select>
                                 </td>
                             </tr>)
