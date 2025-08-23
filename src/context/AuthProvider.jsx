47733c7c5e858e70c3868d1ab30bc4236bebc9ef
MohammadOfficials <mohammadofficial4@gmail.com>
2025-08-23 09:30:49 +0600

    axios added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import auth from '../firebase/firebase.init';
+import axios from 'axios';
 
 const AuthProvider = ({ children }) => {
 
@@ -33,6 +34,24 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            if (currentUser?.email) {
+                const user = { email: currentUser.email }
+
+                axios.post('http://localhost:5000/jwt', user, { withCredentials: true })
+                    .then(res => {
+                        console.log('login token', res.data)
+                        setLoading(false)
+                    })
+            }
+            else {
+                axios.post('http://localhost:5000/logout', {}, {
+                    withCredentials: true
+                })
+                    .then(res => {
+                        console.log('logout', res.data)
+                        setLoading(false)
+                    })
+            }
             console.log(currentUser)
             setLoading(false);
         })
